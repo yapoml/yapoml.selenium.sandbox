7816af14aeb25c66850bec3df362f559e132fba9
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-05-07 14:31:00 +0300

    Demo sources

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+var ya = driver.Ya(options => options.WithBaseUrl("https://github.com"));
+
+ya.SearchPage.Open(query: "yapoml")
+    .Repositories[r => r.Name == "yapoml/yapoml"]
+    .Name.Click();
+
+ya.RepositoryPage.Star.Click(when => when.IsDisplayed());
