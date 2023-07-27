f6a5ff8788efef381ce7fc89dfe883c01ad29b0e
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-07-27 12:23:31 +0300

    Sandbox

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@ var driver = new ChromeDriver();
 var ya = driver.Ya(options => options.WithBaseUrl("https://github.com"));
 
 ya.SearchPage.Open(query: "yapoml")
-    .Repositories
-    .Expect(its => its.IsNotEmpty())
-    [r => r.Name == "yapoml/yapoml"]
+    .Repositories[r => r.Name == "yapoml/yapoml"]
     .Name.Click();
 
 ya.RepositoryPage.Star.Click();
